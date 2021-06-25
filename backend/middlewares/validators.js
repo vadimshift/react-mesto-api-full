c759cdf69bc31548831ba301e244c9be9425fa31
vadimshift <vadimshift@yandex.ru>
2021-06-25 19:54:47 +0500

    исправил ошибку с валидацией

--- backend/middlewares/validators.js
+++ backend/middlewares/validators.js
@@ -5,9 +5,9 @@ const { celebrate, Joi } = require('celebrate');
 
 const validateUserBody = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().required().pattern(new RegExp(/^(?:http(s)?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/)),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(new RegExp(/^(?:http(s)?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/)),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(1),
   }).unknown(true),
@@ -41,8 +41,8 @@ const validateUpdateAvatar = celebrate({
 
 const validateCardBody = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    link: Joi.string().pattern(new RegExp(/^(?:http(s)?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/)),
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(new RegExp(/^(?:http(s)?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/)),
   }).unknown(true),
 });
 
